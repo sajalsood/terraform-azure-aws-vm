aef5891bc599b103579158bb2f5a78d814f681bb
SajalSood <sajal.sood@gmail.com>
2021-03-12 13:04:12 -0500

    aws linux

---
+++ aws-linux/variables.tf
@@ -0,0 +1,10 @@
+variable "profile" {
+  description = "AWS profile name for CLI"
+  default     = "root"
+}
+
+variable "region" {
+  description = "AWS region for infrastructure."
+  default     = "us-east-1"
+}
+
